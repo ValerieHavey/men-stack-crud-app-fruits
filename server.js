3bbfc9b35977960e25bd1e84423d4378833cce0f
Valeriehavey-ga <valeriehavey@gmail.com>
2024-10-07 12:52:33 -0700

    First form complete

--- server.js
+++ server.js
@@ -14,8 +14,31 @@ mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
+const Fruit = require('./models/fruit');
+
+//Let's add our middleware
+app.use(express.urlencoded({ extended: false}));
+
+
+
+// All of our routes
+
 app.get('/', (req, res) => {
     res.render("index.ejs");
+});
+
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.ejs');
+});
+
+app.post('/fruits', async (req, res) => {
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect('/fruits/new');
 });
 
 app.listen(3000, () => {
