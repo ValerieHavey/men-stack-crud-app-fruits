4a602d07d48d682f4e3fedc6110f94470f7d945b
Valeriehavey-ga <valeriehavey@gmail.com>
2024-10-10 12:36:29 -0700

    Create & Delete done

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 const express = require('express');
 const  mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -19,6 +21,10 @@ const Fruit = require('./models/fruit');
 //Let's add our middleware
 app.use(express.urlencoded({ extended: false}));
 
+//Add the middleware for morgan
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+
 
 
 // All of our routes
@@ -27,8 +33,23 @@ app.get('/', (req, res) => {
     res.render("index.ejs");
 });
 
+app.get('/fruits', async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.render('fruits/index.ejs', { fruits: allFruits })
+});
+
 app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs');
+});
+
+app.get('/fruits/:fruitID', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitID);
+    res.render('fruits/show.ejs', { fruit: foundFruit });
+});
+
+app.get('/fruits/:fruitID/edit', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitID);
+    res.render('fruits/edit.ejs', { fruit: foundFruit });
 });
 
 app.post('/fruits', async (req, res) => {
@@ -39,6 +60,23 @@ app.post('/fruits', async (req, res) => {
     }
     await Fruit.create(req.body);
     res.redirect('/fruits/new');
+});
+
+app.put('/fruits/:fruitID', async (req, res) => {
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+
+    await Fruit.findByIdAndUpdate(req.params.fruitID, req.body);
+    res.redirect(`/fruits/${req.params.fruitID}`);
+});
+
+app.delete('/fruits/:fruitID', async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitID);
+    res.redirect('/fruits');
+    //Add error code
 });
 
 app.listen(3000, () => {
